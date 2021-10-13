1f7cea715f672bb1d75fafebbb60c92f9953f272
Prof. Juan Ant. Jimenez <profesorjim@gmail.com>
2021-10-13 18:48:17 +0200

    actualizacion

---
+++ Tema 04/OperadoresLogicos/EjemploAndCondicionesAnidadas.java
@@ -0,0 +1,24 @@
+public class EjemploAndCondicionesAnidadas {
+  /* Este programa localiza números pares entre 10 y 20, ambos incluidos.
+   * Las condiciones de búsqueda son que sea par (resto de la división entre 2 igual a 0) y que esté
+   * 10 y 20
+   * 
+   * Utilizamos anidamiento de ifs para utilizar condiciones simples
+   */
+   
+  public static void main (String[] args) {
+    int x;
+    
+    System.out.print("Introduzca un entero: ");
+    x = Integer.parseInt(System.console().readLine());
+    
+    if (x % 2 == 0) {
+      if (x >= 10) {
+        if (x <= 20) {
+          System.out.println("Este es un número de los que buscamos, par entre 10 y 20");
+        }
+      }
+    }
+    
+  }
+}
