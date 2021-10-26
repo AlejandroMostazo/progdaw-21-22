7cc2ae95d0e7c9280ce665cc38fdd5cb64a61213
profesorjim <profesorjim@gmail.com>
2021-10-26 12:33:44 +0200

    actualizacion

--- Tema 05/Ejercicio15Variacion.java
+++ Tema 05/Ejercicio15Variacion.java
@@ -1,3 +1,5 @@
+public class Ejercicio15Variacion {
+/*
 Escribe un programa que dados dos números, uno real (base) y un entero positivo (exponente),
 saque por pantalla todas las potencias con base sucesivas al numero dado y exponentes entre
 uno y el exponente introducido. No se deben utilizar funciones de exponenciación. Por ejemplo,
@@ -21,15 +23,26 @@ bucle (i): tantas vueltas como b, empezando en 1
       bucle: de "i" vueltas
         resultado = resultado * "a"
       a = a +1;
+*/      
       
-      
-for (int i=1; i<=b; i++) {
-  int resultado = 1;
-  for (int j=1; j<=i; j++) {
-    resultado = resultado * a;
+  public static void main (String[] args) {
+    int a,b;
+    
+    System.out.print("Indique la base: ");
+    a = Integer.parseInt(System.console().readLine());
+    System.out.print("Indique el exponente: ");
+    b = Integer.parseInt(System.console().readLine());
+    
+    for (int i=1; i<=b; i++) {
+      int resultado = 1;
+      for (int j=1; j<=i; j++) {
+        resultado = resultado * a;
+      }
+      System.out.println(a+" elevado a "+i+" es: "+resultado);
+      a = a + 1;
+    }
+    
   }
-  System.out.println(a+" elevado a "+i+" es: "+resultado);
-  a = a + 1;
 }
   
   
